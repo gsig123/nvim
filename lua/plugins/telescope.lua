ea69ab120c631688869a92f0a392851edfe76f93
Bjartur Sigurbergsson <bis@famly.co>
2024-08-22 21:17:26 +0200

    Custom config of telsecope
    Mainly to ignore the gigantic .ci-project... from nuclearfamly

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+return {
+
+  "nvim-telescope/telescope.nvim",
+  keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+  },
+  -- change some options
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "bottom" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      file_ignore_patterns = { "%.ci%-project%-map/" },
+    },
+  },
+}
