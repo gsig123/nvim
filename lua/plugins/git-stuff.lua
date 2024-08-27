baeb27216c5587dbba221e82da9b709b9349d66c
Bjartur Sigurbergsson <bis@famly.co>
2024-08-27 21:30:19 +0200

    Improve git experience

---
+++ lua/plugins/git-stuff.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+  },
+  {
+  "ruanyl/vim-gh-line",
+  config = function()
+    -- Map to open the current line on GitHub
+    vim.api.nvim_set_keymap('n', '<leader>gh', ':OpenInGHFile<CR>', { noremap = true, silent = true })
+  end
+}
+}
